0b2773ff71705995f8ac8a4ca1464508d3194573
tufan8877 <tufandonmezyurek@gmail.com>
2026-01-05 15:59:03 +0100

    main.tsx aktualisieren

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-// Initialize session persistence before anything else
-import "./lib/session-persistence";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
